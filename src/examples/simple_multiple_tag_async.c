f705bc0589c2722acfc8e3a0b199dae89a078164
Kyle Hayes <kyle.hayes@gmail.com>
2024-06-02 11:43:20 -0700

    Adding inttypes.h to provide printf format defines for large ints.

--- src/examples/simple_multiple_tag_async.c
+++ src/examples/simple_multiple_tag_async.c
@@ -31,6 +31,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <inttypes.h>
 #include <stdio.h>
 
 #include <plc_comm.h>
